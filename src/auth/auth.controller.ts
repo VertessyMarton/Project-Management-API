793afc50da6137cabb0927bb2e5711681729ba27
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-04-13 22:49:35 +0200

    feat: implement POST auth/register endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  async createUser(@Body() body: { email: string; password: string }) {
+    return this.authService.createUser(body.email, body.password);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
+import bcrypt from 'node_modules/bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(email: string, password: string): Promise<any> {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await this.userRepository.save({
+      email: email,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+}
