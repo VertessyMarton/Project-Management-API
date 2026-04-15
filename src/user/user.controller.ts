4dde0a249bf203da7095c2829b1e34da9d074dec
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-04-15 19:04:39 +0200

    feat: implement GET users/me endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, UseGuards, Request } from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async getMe(@Request() req) {
+    const { userId } = req.user;
+    return this.userService.getMe(userId);
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async getMe(userId: number) {
+    return await this.userRepository.findOne({
+      where: { id: userId },
+    });
+  }
+}
