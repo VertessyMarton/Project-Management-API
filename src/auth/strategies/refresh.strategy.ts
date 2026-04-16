f3b36e23a40f42afdd81831524652048093e8870
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-04-16 16:02:45 +0200

    feat: add refresh-jwt guard and strategy

--- src/auth/strategies/refresh.strategy.ts
+++ src/auth/strategies/refresh.strategy.ts
@@ -4,12 +4,15 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class RefreshJwtStrategy extends PassportStrategy(
+  Strategy,
+  'refresh-jwt',
+) {
   constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+      secretOrKey: configService.getOrThrow<string>('REFRESH_JWT_SECRET'),
     });
   }
 
