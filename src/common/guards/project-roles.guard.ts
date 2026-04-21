8469f724972944ccaaf788b2a5c20981671a5346
Vértessy Márton Dávid <vertessymarton@mailbox.unideb.hu>
2026-04-21 20:05:29 +0200

    feat: add project roles guard

---
+++ src/common/guards/project-roles.guard.ts
@@ -0,0 +1,43 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  NotFoundException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ProjectRoles } from '../decorators/project-roles.decorator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProjectMembers } from 'src/project/entities/project-members.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ProjectRolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    @InjectRepository(ProjectMembers)
+    private projectMemberRepository: Repository<ProjectMembers>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const roles = this.reflector.get(ProjectRoles, context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const request = context.switchToHttp().getRequest();
+
+    const membership = await this.projectMemberRepository.findOne({
+      where: {
+        user: { id: request.user.id },
+        project: { id: request.params.projectId },
+      },
+    });
+
+    if (!membership) {
+      throw new NotFoundException('Resource not found');
+    }
+
+    console.log(membership.role);
+
+    return roles.includes(membership.role);
+  }
+}
